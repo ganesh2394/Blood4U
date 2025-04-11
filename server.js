a222e2a3c87e0d769e869a5659475350bdaf35a5
Ganesh Prasad <gp2581739@gmail.com>
2025-04-11 11:23:56 +0530

    Modified package.json and server.js files

--- server.js
+++ server.js
@@ -27,6 +27,15 @@ app.use("/api/test", testRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/inventory", inventoryRoutes);
 
+// Serve static files in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 8080;
 const DEV_MODE = process.env.DEV_MODE;
 app.listen(PORT, () =>
